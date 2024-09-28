bfa6d1ed21dbc6ff7eb8073d97528fef956c7434
21120036 <thienan2003bt@gmail.com>
2024-09-28 20:40:42 +0700

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Container } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
+import UserPage from "./pages/UserPage"
+
+function App() {
+  return (
+    <Container maxW="620px">
+		<Routes>
+			<Route path='/:username' element={<UserPage />}/>
+			<Route path='/:username/post/:pid' element={<UserPage />}/>
+		</Routes>
+
+    </Container>
+  )
+}
+
+export default App
---
+++ src/pages/UserPage.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@chakra-ui/react';
+
+function UserPage() {
+    return (
+        <div>
+            <h1>Hello there!</h1>     	
+            <Button>Click me please</Button>
+        </div>
+    );
+}
+
+export default UserPage;
